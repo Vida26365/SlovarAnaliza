ef38445034bcd17436ad4c73f865dbab3530dd04
83433121 <53379198+83433121@users.noreply.github.com>
2024-06-27 23:08:29 +0200

    uredila main. preizkušam, če gitignore dela

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from pridobivanje_podatkov import *
 
 
-def main(downlovdaj=True, ):
+def main(downlovdaj=True, pocsvjaj=True):
     #spremenljivke____________________________________________________
     mapa = "podatki"
     html = "html.html"
-    csv = "csv.scv"
+    csv = "csv.csv"
+    url = "https://www.fran.si/iskanje?View=1&Query=*&All=*&FilteredDictionaryIds=133"
+
     
     pot = os.path.join(mapa, html)
     if downlovdaj or not os.path.exists(pot):
@@ -13,28 +15,15 @@ def main(downlovdaj=True, ):
     else:
         print("Datoteka html že obstaja")
     
+    pot = os.path.join(mapa, csv)
+    if pocsvjaj or not os.path.exists(pot):
+        tekst = file_to_string(mapa, html)
+        dict_to_csv(regexanje(tekst), mapa, csv)
+    else:
+        print("datoteka csv že obstaja")
     
     
     
-    
-    url = "https://www.fran.si/iskanje?View=1&Query=*&All=*&FilteredDictionaryIds=133"
-
-    url_to_file(url, "datoteke", "html.html")
-    test2 = file_to_string("datoteke", "html.html")
-    dict_to_csv(regexanje(test2),"datoteke", "csv.csv")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
     main(False)
+
