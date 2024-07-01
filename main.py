bb37576c75e39eb7506d65a3299dcb92aaae67ae
83433121 <53379198+83433121@users.noreply.github.com>
2024-07-01 12:39:37 +0200

    uredila main

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import time
 
 def main(downlovdaj=True, pocsvjaj=True):
     #spremenljivke______________________________________________________________________________________________
+    
+    #spremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+    od = 1
+    do = 3 #vključno z    (do 4884)
+    
+    #nespremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
     mapa_slovar = "slovarski_podatki"
     html = "html.html"
     csv = "csv.csv"
@@ -12,9 +18,6 @@ def main(downlovdaj=True, pocsvjaj=True):
     file_hitrosti = "podatki.csv"
 
     url = lambda n: f"https://www.fran.si/iskanje?page={n}&View=1&Query=*&All=*&FilteredDictionaryIds=133" 
-    
-    od = 1
-    do = 3 #vključno z    (do 4884)
     
     
     #program____________________________________________________________________________________________________
