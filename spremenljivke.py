df00816b2b04faa9301daa2f4f658aa031285f60
83433121 <53379198+83433121@users.noreply.github.com>
2024-07-05 01:12:47 +0200

    dodala kompleksne spremenljivke

--- spremenljivke.py
+++ spremenljivke.py
@@ -1,6 +1,8 @@
+import pandas as pd
+import os
 #spremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-od = 1
-do = 3 #vključno z    (do 4884)
+prva_stran = 1
+zadnja_stran = 10 #vključno z    (do 4884)
 
 #nespremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 mapa_slovar = "slovar"
@@ -12,3 +14,8 @@ file_hitrosti = "podatki.csv"
 
 url = lambda n: f"https://www.fran.si/iskanje?page={n}&View=1&Query=*&All=*&FilteredDictionaryIds=133" 
 
+#spremenljivke za analizo_________________________________________
+pd.set_option("display.max_rows", 10)
+# %matplotlib inline
+pot_slovar = os.path.join(mapa_slovar, csv)
+razpredelnica = pd.read_csv(pot_slovar)
