cb34d6eae8a91418f199610f96882e8dad48545d
83433121 <53379198+83433121@users.noreply.github.com>
2024-07-14 22:38:13 +0200

    uredila in uskladila spremenljivke

--- pridobivanje_podatkov.py
+++ pridobivanje_podatkov.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-import re
+# import re  #'#'#'##'#'#'#'#'#'#'#'#'#'##'#'#'#'#'#'#'#''##'#'#
 import os
 import csv
-import time
+import time ##############################################
 import pandas as pd
 
 
@@ -15,10 +15,9 @@ def url_to_file(url,  mapa, file, način = "a"):
         headers = {"User-agent":"Chrome/124.0.6367.207"}
         vsebina = requests.get(url, headers=headers)
     except requests.exceptions.RequestException:
-        print ("spletna stran ni dosegljiva")
+        print (f"spletna {url} stran ni dosegljiva")
         return None
     tekst = vsebina.text
-    
     
     os.makedirs(mapa, exist_ok=True)
     pot = os.path.join( mapa, file)
@@ -26,59 +25,61 @@ def url_to_file(url,  mapa, file, način = "a"):
         file.write(tekst)
     return
 
-def file_to_string( mapa, file):
+def file_to_string(mapa, file):
     pot = os.path.join( mapa, file)
     with open(pot, "r", encoding="utf-8") as file:
         text = file.read()
     return text
 
 def vse_strani_to_html(od, do, url, mapa, file): #url je funkcija
-    zač = time.time()
-    print("v procesu pridobivanja html-ja")
-    for i in range(od, do+1):
+    zač = time.time()#########################################################33
+    print("v procesu pridobivanja html-ja...")
+    
+    print("stran", od)
+    url_to_file(url(od), mapa, file, "w") #prva stran je posebna, ker mora prva stran začeti pisati datoteko od začetka, namesto dodajati, kot to delajo naslednje strani
+    for i in range(od+1, do+1):
         način = "a"
-        if i == od:
-            način = "w"
         print("stran", i)
-        url_to_file(url(i), mapa, file, način )
-    return time.time()-zač
+        url_to_file(url(i), mapa, file, "a")
+    return time.time()-zač################################################33
 
 # csv_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-def html_to_csv( mapa, html, csv):
-    zač = time.time()
-    print("v procesu pridobivanja posatkov iz htmlja v csv")
-    tekst = file_to_string( mapa, html)
+def html_to_csv(mapa, html, csv):
+    zač = time.time()####################################
+    print("v procesu pridobivanja posatkov iz htmlja v csv...")
+    tekst = file_to_string(mapa, html)
     dict_to_csv(regexanje(tekst), mapa, csv, "w")
-    return time.time()-zač
+    return time.time()-zač ######################################3333
 
 
-def dict_to_csv(slovar,  mapa, file, način="w", naslov=True):
+def dict_to_csv(slovar,  mapa, file, način="w", naslov=True): ##################################################
     pot = os.path.join( mapa, file)
-    if not os.path.exists(pot):
+    if not os.path.exists(pot): #to bom leahko asneje spremenila#################################################
         naslov = True
-    
     
     os.makedirs(mapa, exist_ok=True)
     oznake = list(slovar[0].keys())
     with open(pot, način, encoding="utf-8") as file:
         pisatelj = csv.DictWriter(file, fieldnames=oznake)
-        if naslov == True:
+        if naslov == True: #############################################################
             pisatelj.writeheader()
         for stvar in slovar:
             pisatelj.writerow(stvar)
     return
 
 
-def podatki_to_dict(t1, t2, od, do):
+def podatki_to_dict(t1, t2, od, do): ##############################################################3
     št = do-od+1
     if t1 == None: html_hitrost = 1
     html_hitrost = št/t1
     csv_hitrost = št/t2
     return [{"čas_html":t1, "čas_csv":t2, "strani": št, "html_hitrost":html_hitrost, "csv_hitrost":csv_hitrost}]
 
-def podatki_to_csv( mapa, file, od, do, t1=None, t2= None):
+def podatki_to_csv( mapa, file, od, do, t1=None, t2= None): ###################################################################
     slovar = podatki_to_dict(t1, t2, od, do)
     dict_to_csv(slovar,  mapa, file, "a", naslov=False)
+
+
 
 
 # pridobi_type_funkcije_____________________________________________________________________________________________
@@ -86,12 +87,14 @@ def regexanje(tekst):
     print("Regexanje...")
     soup = BeautifulSoup(tekst, "html5lib") #poglej si kaj naredi "html5lib"
     seznam = []
-    tabela = soup.find_all("div", attrs={"class":"entry-content"}) #najde posamezen  
+    tabela = soup.find_all("div", attrs={"class":"entry-content"}) #najde posamezeo besedo njej pripradajoče stvari 
     
     for odstavek in tabela:
         slovar = {}
+        #pridobivanje_stvari____________________________________________________________________________________
         slovar["ime"] = odstavek.a.text
         
+        #oblika_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
         listek = set()
         for span in odstavek.find_all("span", attrs={"title":"Oblika"}):
             
@@ -99,8 +102,9 @@ def regexanje(tekst):
                 listek.add(c)
         slovar["oblika"] = listek
         
+        #besedne_vrste_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
         def pomozna_definicija(tag):
-            vrste = ["samostalnik", "pridevnik", "glagol", "veznik", "členek", "prislov", "medmet"]
+            vrste = ["samostalnik", "pridevnik", "glagol", "veznik", "členek", "prislov", "medmet"] #######################################
             if not tag.has_attr("title"):
                 return False
             for vrsta in vrste:
@@ -116,20 +120,20 @@ def regexanje(tekst):
             else:
                 slovar["vrsta"] = None
         
+        #izgovor_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
         listek = set()
         for span in odstavek.find_all(attrs={"title":"Izgovor"}):
             listek.add(span.text)
         slovar["izgovor"] = listek
         
+        #tonemski_naglas_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
         listek = set()
         for span in odstavek.find_all(attrs={"title":"Tonemski naglas"}):
             listek.add(span.text)
         slovar["tonemski naglas"] = listek
         
-
         seznam.append(slovar)
-    
-    print(seznam, len(seznam))
+        
     return seznam
 
 
--- spremenljivke.py
+++ spremenljivke.py
@@ -1,21 +1,25 @@
 import pandas as pd
 import os
-#spremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+#spremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 prva_stran = 1
 zadnja_stran = 10 #vključno z    (do 4884)
 
-#nespremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+#nespremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 mapa_slovar = "slovar"
 html = "html.html"
 csv = "besede.csv"
+mapa_stalni_slovar = "stalni_slovar"
 
-mapa_hitrosti = "program" #posebna mapa, ker se ta ne briše
-file_hitrosti = "podatki.csv"
+
+mapa_hitrosti = "program" #posebna mapa, ker se ta ne briše ###################################3333
+file_hitrosti = "podatki.csv"##########################################
 
 url = lambda n: f"https://www.fran.si/iskanje?page={n}&View=1&Query=*&All=*&FilteredDictionaryIds=133" 
 
-#spremenljivke za analizo_________________________________________
-pd.set_option("display.max_rows", 10)
-# %matplotlib inline
-pot_slovar = os.path.join(mapa_slovar, csv)
-razpredelnica = pd.read_csv(pot_slovar)
+#obdelane_spremenljivke_________________________________________________________________________________________
+pot_slovar_csv = os.path.join(mapa_slovar, csv)
+pot_slovar_html = os.path.join(mapa_slovar, html)
+
+
+#dodatno________________________________________________________________________________________________________
+abeceda = "abcčdefghijklmnoprsštuvzž"
