8c573982e5cf64edf909b8505d453aaeaf3ee415
83433121 <53379198+83433121@users.noreply.github.com>
2024-07-20 01:25:12 +0200

    dodala delitej in odstanila nepotrebne komentarje

--- spremenljivke.py
+++ spremenljivke.py
@@ -2,12 +2,15 @@ import pandas as pd
 import os
 #spremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 prva_stran = 1
-zadnja_stran = 5 #vključno z    (do 4884)
+zadnja_stran = 4884 #vključno z    (do 4884)
+
+delitelj = 100
+
 
 #nespremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
 mapa_slovar = "slovar"
-html = lambda n: f"html{n//100}.html"
-# html = "html.html"
+html = lambda n: f"html{n}.html"
 csv = "besede.csv"
 parquet = "parquet.parquet"
 mapa_stalni_slovar = "stalni_slovar"
@@ -18,10 +21,6 @@ file_hitrosti = "podatki.csv"##########################################
 
 url = lambda n: f"https://www.fran.si/iskanje?page={n}&View=1&Query=*&All=*&FilteredDictionaryIds=133" 
 
-#obdelane_spremenljivke_________________________________________________________________________________________
-pot_slovar_csv = os.path.join(mapa_slovar, csv)
-pot_slovar_parquet = os.path.join(mapa_slovar, parquet)
-# pot_slovar_html = os.path.join(mapa_slovar, html)
 
 
 #dodatno________________________________________________________________________________________________________
