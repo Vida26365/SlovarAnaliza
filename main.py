fa9dd9822c2a2d19fb5c96548c0c9b6db80f3336
83433121 <53379198+83433121@users.noreply.github.com>
2024-07-19 15:41:21 +0200

    dodala nazaj časove

--- main.py
+++ main.py
@@ -11,23 +11,26 @@ def main(downlovdaj=False, pocsvjaj=False, od=prva_stran, do=zadnja_stran):
     #pridobivanje_podatkov____________________________________________________________________________________________________
 
     #html_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-    if downlovdaj or not os.path.exists(pot_slovar_html):
-        vse_strani_to_html(od, do, url, mapa_slovar, html)
+    if downlovdaj or not os.path.exists(mapa_slovar):
+        t1 = vse_strani_to_html(od, do, url, mapa_slovar, html)
     else:
         print("Datoteka html že obstaja")
+        t1 = None
     
     #csv_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-    if pocsvjaj or not os.path.exists(pot_slovar_csv):
-        html_to_csv(mapa_slovar, html, csv)
+    if pocsvjaj or not os.path.exists(mapa_slovar):
+        t2 = html_to_csv(mapa_slovar, html, csv, od, do)
+        # t2 = html_to_parquet(mapa_slovar, html, parquet)
     else:
         print("datoteka csv že obstaja")
+        t2 = None
     
     #ostali podatki_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-    # podatki_to_csv( mapa_hitrosti, file_hitrosti, od, do, t1, t2)
+    podatki_to_csv(mapa_hitrosti, file_hitrosti, od, do, t1, t2)
 
     print("končano")
 
 #klicanje main__________________________________________________________________________________________________
 if __name__ == '__main__':
-    main()
+    main(False, True)
 
