fb152c8dce4c50d2df25c1a550591e05747d4dc7
83433121 <53379198+83433121@users.noreply.github.com>
2024-07-20 00:47:30 +0200

    dodala pomožno funkcijo obstaja() in dodala, da se vse prejšnje datoteke ob ponovnem downloadu zbrišejo

--- main.py
+++ main.py
@@ -3,24 +3,34 @@ import time
 from spremenljivke import *
 
 
-def main(downlovdaj=False, pocsvjaj=False, od=prva_stran, do=zadnja_stran):
+def obstaja(mapa, datoteka):
+    for file in os.listdir(mapa):
+        if "."+datoteka in file:
+            return True
+    return False
+
+
+def main(downlovdaj=False, pocsvjaj=False, od=prva_stran, do=zadnja_stran, mapa_slovar=mapa_slovar):
     if do > 4884:
         do = 4884
     if od < 1:
         od = 1
     #pridobivanje_podatkov____________________________________________________________________________________________________
 
-    #html_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-    if downlovdaj or not os.path.exists(mapa_slovar):
+    #html_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_   
+    if downlovdaj or not obstaja(mapa_slovar, "html"):
+        for i in range(od, do+1):
+            pot_html = os.path.join(mapa_slovar, html(i))
+            if os.path.exists(pot_html):
+                os.remove(pot_html)
         t1 = vse_strani_to_html(od, do, url, mapa_slovar, html)
     else:
         print("Datoteka html že obstaja")
         t1 = None
     
     #csv_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-    if pocsvjaj or not os.path.exists(mapa_slovar):
+    if pocsvjaj or not obstaja(mapa_slovar, "csv"):
         t2 = html_to_csv(mapa_slovar, html, csv, od, do)
-        # t2 = html_to_parquet(mapa_slovar, html, parquet)
     else:
         print("datoteka csv že obstaja")
         t2 = None
@@ -32,5 +42,5 @@ def main(downlovdaj=False, pocsvjaj=False, od=prva_stran, do=zadnja_stran):
 
 #klicanje main__________________________________________________________________________________________________
 if __name__ == '__main__':
-    main(False, True)
+    main(True, True)
 
