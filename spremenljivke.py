cc14076d6504aaf4ad5c4231b8694d8604ec7f35
83433121 <53379198+83433121@users.noreply.github.com>
2024-07-19 15:42:13 +0200

    dala stran pot_slovar_htmk in naredila html funkcijo

--- spremenljivke.py
+++ spremenljivke.py
@@ -2,12 +2,14 @@ import pandas as pd
 import os
 #spremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 prva_stran = 1
-zadnja_stran = 10 #vključno z    (do 4884)
+zadnja_stran = 5 #vključno z    (do 4884)
 
 #nespremenljive_spremenljivke_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 mapa_slovar = "slovar"
-html = "html.html"
+html = lambda n: f"html{n//100}.html"
+# html = "html.html"
 csv = "besede.csv"
+parquet = "parquet.parquet"
 mapa_stalni_slovar = "stalni_slovar"
 
 
@@ -18,7 +20,8 @@ url = lambda n: f"https://www.fran.si/iskanje?page={n}&View=1&Query=*&All=*&Filt
 
 #obdelane_spremenljivke_________________________________________________________________________________________
 pot_slovar_csv = os.path.join(mapa_slovar, csv)
-pot_slovar_html = os.path.join(mapa_slovar, html)
+pot_slovar_parquet = os.path.join(mapa_slovar, parquet)
+# pot_slovar_html = os.path.join(mapa_slovar, html)
 
 
 #dodatno________________________________________________________________________________________________________
